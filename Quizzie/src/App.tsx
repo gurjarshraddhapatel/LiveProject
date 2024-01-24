3a27340de5e3cc3ffd59d2c44c0ddc06199bfccc
Sankalp70 <sankalprt658@gmail.com>
2024-01-24 18:37:11 +0530

    crreate new file dasboard

--- Quizzie/src/App.tsx
+++ Quizzie/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Home from './component/index';
+import Dasboard from './component/dasboard';
 function App() {
 
 
@@ -7,7 +8,7 @@ function App() {
     <>
     
     <Home/>
-    
+    <Dasboard/>
     </>
   )
 }
---
+++ Quizzie/src/component/dasboard.tsx
@@ -0,0 +1,11 @@
+// /import React from 'react'
+
+const Dasboard = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default Dasboard

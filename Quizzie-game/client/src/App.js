00d22cc28c3f07af2a77f8cf55d1331bb9bb1f62
gurjarshraddhapatel <gurjarshraddha100@gmail.com>
2024-01-27 00:35:23 +0530

    update

--- Quizzie-game/client/src/App.js
+++ Quizzie-game/client/src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import Dashboard from "./components/Home/Dashboard/Dashboard";
-import MalePage from './components/Home/Dashboard/MalePage'; // Adjust the path as needed
-import FemalePage from './components/Home/Dashboard/FemalePage'; // Adjust the path as needed
+// import QuizCompletion from './components/QizCompletion/QuizCompletion';
+import QuizCompletion from './components/Home/QuizCompletion/QuizCompletion';
+import Question from './components/Home/questions/Questions';
+
 
 function App() {
   return (
@@ -14,8 +16,9 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/home" element={<Dashboard />} />
-          <Route path="/male" exact component={MalePage} />
-          <Route path="/female" exact component={FemalePage} />
+          <Route path="/quiz" exact component={QuizCompletion} />
+          <Route path="/question" exact component={Question} />
+
         </Routes>
       </BrowserRouter>
     </div>
